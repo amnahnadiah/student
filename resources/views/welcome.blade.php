420a2bcdf5087c053c6c488f6fe7e426f230ce0c
rabiatulrosly <61118533+rabiatulrosly@users.noreply.github.com>
2021-03-16 11:52:39 +0800

    ubah kuasa dua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    weeeee
+                    hehehehe
                 </div>
 
                 <div class="links">
