a51582f4e03497ad151d3992cfaabdd0aabc0a57
amnahnadiah <amnahnadiahs@gmail.com>
2021-03-17 15:29:02 +0800

    tukar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    hehehehe
+                    hehehehgrgfjgd
                 </div>
 
                 <div class="links">
