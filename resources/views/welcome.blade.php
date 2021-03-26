71539fb1e5fb3f9a4c98b3f0c4589bed46597c2f
amnahnadiah <amnahnadiahs@gmail.com>
2021-03-26 15:00:20 +0800

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,47 +65,40 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+        
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ Request::root() }}/profile">Profile</a>
-                    <a href="{{ Request::root() }}/teacher">Teacher</a>
-                    <a href="{{ Request::root() }}/student">Student</a>
-                    <a href="{{ Request::root() }}/guardian">Guardian</a>
-                    <a href="{{ Request::root() }}/school">School</a>
                     <a href="{{ Request::root() }}/subject">Subject</a>
-                    <a href="{{ Request::root() }}/edulevel">Education Level</a>
-                    <a href="{{ Request::root() }}/room">Room</a>
-                    <a href="{{ Request::root() }}/alamat">Address</a>
+                    <a href="{{ Request::root() }}/teacher">Teacher</a>
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                            
-                        @endif
+                        <a href="{{ Request::root() }}/profile">Profile</a>
+                        <a href="{{ Request::root() }}/student">Student</a>
+                        <a href="{{ Request::root() }}/guardian">Guardian</a>
+                        <a href="{{ Request::root() }}/school">School</a>
+                        <a href="{{ Request::root() }}/edulevel">Education Level</a>
+                        <a href="{{ Request::root() }}/room">Room</a>
+                        <a href="{{ Request::root() }}/alamat">Address</a>
                     @endauth
                     
-                    
+                </div>
+
+                <div class="content">
+                    <div class="title m-b-md">
+                        Tuition
+                    </div>
+
+                    <div class="links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Tuition
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
     </body>
 </html>
