1a73580279fbc4454c5f01bd9519a9ba3d5229fc
amnahnadiah <amnahnadiahs@gmail.com>
2021-04-02 10:51:33 +0800

    edit route

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,6 +18,6 @@ class Admin
         if(auth()->user()->is_admin == 1){
             return $next($request);
         }
-            return redirect('home')->with('error','You have no admin access');
+            return redirect('/home')->with('error','You have no admin access');
     }
 }
